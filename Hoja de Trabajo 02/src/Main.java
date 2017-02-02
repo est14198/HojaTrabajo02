b9b3c138ede06d78237be7dde62f0da4ee7158fc
Maria Fernanda Estrada <est14198@uvg.edu.gt>
2017-02-02 12:16:35 -0600

    Interfaz Calculadora

--- Hoja de Trabajo 02/src/Main.java
+++ Hoja de Trabajo 02/src/Main.java
@@ -1,4 +1,16 @@
 /**
+ * Main.java
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructura de Datos Seccion 10
+ * Hoja de Trabajo 02
+ * Maria Fernanda Estrada 14198
+ * Christopher Sandoval 13660
+ * 6 Enero 2017
+ */
+
+
+
+/**
  *
  * @author FER ESTRADA
  */
---
+++ Hoja de Trabajo 02/src/iCalculadora.java
@@ -0,0 +1,23 @@
+/**
+ * iCalculadora.java
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructura de Datos Seccion 10
+ * Hoja de Trabajo 02
+ * Maria Fernanda Estrada 14198
+ * Christopher Sandoval 13660
+ * 6 Enero 2017
+ */
+
+public interface iCalculadora {
+	
+	public double operar (String n);
+	
+	public double suma (double a, double b);
+	
+	public double resta (double a, double b);
+	
+	public double multiplicacion (double a, double b);
+	
+	public double division (double a, double b);
+
+}
