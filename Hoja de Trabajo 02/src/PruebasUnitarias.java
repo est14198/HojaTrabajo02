197971d22505855660f5aa267b2fd45214c5d914
Christopher Sandoval <san13660@uvg.edu.gt>
2017-02-06 20:21:37 -0600

    FINAL FINAL

--- Hoja de Trabajo 02/src/PruebasUnitarias.java
+++ Hoja de Trabajo 02/src/PruebasUnitarias.java
@@ -16,33 +16,55 @@ import org.junit.Test;
 public class PruebasUnitarias {
 
 	@Test
-	public void Division() {
-		Calculadora calculadora = new Calculadora();
+	public void division() {
+		iCalculadora calculadora = new Calculadora();
 		assertEquals("Division de 10/2",calculadora.division(10, 2),5,0.001);
 	}
 	
 	@Test
-	public void Multiplicacion() {
-		Calculadora calculadora = new Calculadora();
+	public void multiplicacion() {
+		iCalculadora calculadora = new Calculadora();
 		assertEquals("Multiplicacion de 10*2",calculadora.multiplicacion(10, 2),20,0.001);
 	}
 	
 	@Test
-	public void Suma() {
-		Calculadora calculadora = new Calculadora();
+	public void suma() {
+		iCalculadora calculadora = new Calculadora();
 		assertEquals("Suma de 10+2",calculadora.suma(10, 2),12,0.001);
 	}
 	
 	@Test
-	public void Resta() {
-		Calculadora calculadora = new Calculadora();
+	public void resta() {
+		iCalculadora calculadora = new Calculadora();
 		assertEquals("Resta de 10-2",calculadora.resta(10, 2),8,0.001);
 	}
 	
 	@Test
-	public void OperacionBasica() {
-		Calculadora calculadora = new Calculadora();
+	public void operacionBasica() {
+		iCalculadora calculadora = new Calculadora();
 		assertEquals("Operacion: 1 1 +",calculadora.operar(" 1 1 +"),2,0.001);
+	}
+	
+	@Test
+	public void push() {
+		iPila<Double> pila = new PilaVector<Double>(1);
+		pila.push(2.0);
+		assertEquals("Push de un numero", pila.isEmpty(),false);
+	}
+	
+	@Test
+	public void pop() {
+		iPila<Double> pila = new PilaVector<Double>(1);
+		pila.push(2.0);
+		assertEquals("Pop de un numero", pila.pop(),2.0,0.001);
+	}
+	
+	@Test
+	public void isEmpty() {
+		iPila<Double> pila = new PilaVector<Double>(1);
+		pila.push(2.0);
+		pila.push(3.0);
+		assertEquals("Verificar si esta vacio", pila.isEmpty(),false);
 	}
 
 	
